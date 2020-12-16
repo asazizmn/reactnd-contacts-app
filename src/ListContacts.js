6de73b531088d5d7e325b7bd04a7f0baa707eac4
S. Aziz <asazizmn@gmail.com>
2020-12-16 17:22:07 +0800

    add ListContact contents for display

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,16 +4,36 @@ import React, { Component } from 'react';
 
 class ListContacts extends Component {
     render() {
-
-        // DEBUG !!! ////////////////////
-        // console.log('Props', this.props);
-
-        const contacts = this.props.contacts;
-
         return (
             <ol className='contact-list'>
-                {contacts.map(contact => (
-                    <li key={contact.id}>{contact.name}</li>
+                {this.props.contacts.map(contact => (
+                    <li key={contact.id} className='contact-list-item'>
+                        {
+                            // generally when images are part of the actual content, use <img>
+                            // but when images are not part of the main content, simply use background-image
+                            // <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
+
+                            // since the avatar is actually part of the content here
+                            // ... it makes sense to use 'img', as opposed to 'background-image'
+                        }
+                        <div className='contact-avatar'>
+                            <img
+                                src={contact.avatarURL}
+                                style={{ borderRadius: '50%' }}
+                                alt={contact.name}
+                                title={contact.name}
+                                height='100%'
+                                width='100%'
+                            />
+                        </div>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>@{contact.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
                 ))}
             </ol>
         )
