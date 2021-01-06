3f2f6437e60e0eb12976605378ac8a59b246e24d
S. Aziz <asazizmn@gmail.com>
2021-01-06 13:56:43 +0800

    add contacts to App state (internal)

--- src/App.js
+++ src/App.js
@@ -2,39 +2,41 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
 
-// please note that the static data has been placed outside the component
-// ... and not inside it, especially the render method
-// ... otherwise, the array would be instantiated with every component render
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
-
 
 class App extends Component {
+
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  };
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
 }
+
+
 
 export default App;
