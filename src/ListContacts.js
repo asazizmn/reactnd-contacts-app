69a904dff7eff063a3687cccdaedd0e0c9e55452
S. Aziz <asazizmn@gmail.com>
2021-01-25 15:17:31 +0800

    refactor functional ListContacts component into a class one

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,61 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 
 
-function ListContacts(props) {
-    return (
-        <ol className='contact-list'>
-            {props.contacts.map(contact => (
-                <li key={contact.id} className='contact-list-item'>
-                    {
-                        // generally when images are part of the actual content, use <img>
-                        // but when images are not part of the main content, simply use background-image
-                        // <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
+class ListContacts extends Component {
 
-                        // since the avatar is actually part of the content here
-                        // ... it makes sense to use 'img', as opposed to 'background-image'
-                    }
-                    <div className='contact-avatar'>
-                        <img
-                            src={contact.avatarURL}
-                            style={{ borderRadius: '50%' }}
-                            alt={contact.name}
-                            title={contact.name}
-                            height='100%'
-                            width='100%'
-                        />
-                    </div>
+    // this is how you add PropTypes to a class component
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onContactDelete: PropTypes.func.isRequired
+    }
 
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>@{contact.handle}</p>
-                    </div>
 
-                    <button
-                        className='contact-remove'
-                        onClick={
+    render() {
+        return (
+            <ol className='contact-list'>
+                {this.props.contacts.map(contact => (
+                    <li key={contact.id} className='contact-list-item'>
+                        {
+                            // generally when images are part of the actual content, use <img>
+                            // but when images are not part of the main content, simply use background-image
+                            // <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
 
-                            // please note that it is not possible to call functions directly here, in react
-                            // rather it is important to provide a function definition instead
-                            () => props.onContactDelete(contact)
+                            // since the avatar is actually part of the content here
+                            // ... it makes sense to use 'img', as opposed to 'background-image'
                         }
-                    >
-                        Remove
+                        <div className='contact-avatar'>
+                            <img
+                                src={contact.avatarURL}
+                                style={{ borderRadius: '50%' }}
+                                alt={contact.name}
+                                title={contact.name}
+                                height='100%'
+                                width='100%'
+                            />
+                        </div>
+
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>@{contact.handle}</p>
+                        </div>
+
+                        <button
+                            className='contact-remove'
+                            onClick={
+
+                                // please note that it is not possible to call functions directly here, in react
+                                // rather it is important to provide a function definition instead
+                                () => this.props.onContactDelete(contact)
+                            }
+                        >
+                            Remove
                     </button>
-                </li>
-            ))}
-        </ol>
-    )
+                    </li>
+                ))}
+            </ol>
+        )
+    }
 }
 
 
 
-// props validation definition
-ListContacts.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onContactDelete: PropTypes.func.isRequired
-}
+// // props validation definition
+// // this is how you add PropTypes to a functional component
+// ListContacts.propTypes = {
+//     contacts: PropTypes.array.isRequired,
+//     onContactDelete: PropTypes.func.isRequired
+// }
 
 
 
