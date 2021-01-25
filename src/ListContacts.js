18ddaa556c05e314a595ec702658f6233a20acfb
S. Aziz <asazizmn@gmail.com>
2021-01-25 16:48:50 +0800

    add query field

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -19,49 +19,77 @@ class ListContacts extends Component {
     }
 
 
+    // query event handler definition
+    updateQuery = query => {
+        this.setState(() => ({
+
+            // removes whitespace from both ENDS of a string
+            query: query.trim()
+        }))
+    }
+
+
     render() {
         return (
-            <ol className='contact-list'>
-                {this.props.contacts.map(contact => (
-                    <li key={contact.id} className='contact-list-item'>
-                        {
-                            // generally when images are part of the actual content, use <img>
-                            // but when images are not part of the main content, simply use background-image
-                            // <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input
+                        type='text'
+                        className='search-contacts'
+                        placeholder='Search Contacts'
 
-                            // since the avatar is actually part of the content here
-                            // ... it makes sense to use 'img', as opposed to 'background-image'
-                        }
-                        <div className='contact-avatar'>
-                            <img
-                                src={contact.avatarURL}
-                                style={{ borderRadius: '50%' }}
-                                alt={contact.name}
-                                title={contact.name}
-                                height='100%'
-                                width='100%'
-                            />
-                        </div>
+                        // this is what turns this component into a "controlled component"
+                        // it's basically when form 
+                        value={this.state.query}
+                        onChange={event => this.updateQuery(event.target.value)}
+                    />
+                </div>
 
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>@{contact.handle}</p>
-                        </div>
+                <ol className='contact-list'>
+                    {this.props.contacts.map(contact => (
+                        <li key={contact.id} className='contact-list-item'>
+                            {
+                                // generally when images are part of the actual content, use <img>
+                                // but when images are not part of the main content, simply use background-image
+                                // <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
 
-                        <button
-                            className='contact-remove'
-                            onClick={
-
-                                // please note that it is not possible to call functions directly here, in react
-                                // rather it is important to provide a function definition instead
-                                () => this.props.onContactDelete(contact)
+                                // since the avatar is actually part of the content here
+                                // ... it makes sense to use 'img', as opposed to 'background-image'
                             }
-                        >
-                            Remove
+                            <div className='contact-avatar'>
+                                <img
+                                    src={contact.avatarURL}
+                                    style={{ borderRadius: '50%' }}
+                                    alt={contact.name}
+                                    title={contact.name}
+                                    height='100%'
+                                    width='100%'
+                                />
+                            </div>
+
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>@{contact.handle}</p>
+                            </div>
+
+                            <button
+                                className='contact-remove'
+                                onClick={
+
+                                    // please note that it is not possible to call functions directly here, in react
+                                    // rather it is important to provide a function definition instead
+                                    () => this.props.onContactDelete(contact)
+                                }
+                            >
+                                Remove
                     </button>
-                    </li>
-                ))}
-            </ol>
+                        </li>
+                    ))}
+                </ol>
+
+            </div>
+
+
         )
     }
 }
