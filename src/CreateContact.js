69941d6b8052c6df12739a0bd2c0ad1b5f0e4afe
A.S. Aziz <asazizmn@gmail.com>
2021-03-22 17:38:37 +0800

    handle form submission

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serialize from 'form-serialize';
 
 
 
 class CreateContact extends Component {
+
+    handleSubmit = event => {
+        event.preventDefault();
+        
+        // `form-serialize` allows data to be transformed into a format that can be
+        // ... transmitted (ie query string), or stored
+        // [https://www.npmjs.com/package/form-serialize]
+        const obj = serialize(event.target, {
+            
+            // `hash` for output as a js object (instead of the default url encoded)
+            // obj -> { foo: 'bar' } (instead of str -> "foo=bar")
+            hash: true
+        });
+
+        // `onCreateContact` is yet to be created
+        if (this.props.onCreateContact) {
+            this.props.onCreateContact(obj);
+        }
+    }
+
     render() {
         return (
             <div>
-                <Link exact to='/' className="close-create-contact">
+                <Link to='/' className="close-create-contact">
                     Close
                 </Link>
 
-                <form className="create-contact-form">
+                <form onSubmit={this.handleSubmit} className="create-contact-form">
                     <ImageInput
                         name='avatarURL'
                         maxHeight={64}
