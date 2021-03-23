990922a7def70c8c80f78de76adc294a1ff30860
A.S. Aziz <asazizmn@gmail.com>
2021-03-23 16:55:49 +0800

    make react router related imports more apparent

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+
+import { Link } from 'react-router-dom';
 import serialize from 'form-serialize';
 
 
